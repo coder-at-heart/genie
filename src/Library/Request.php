3d8de73e19df46ff447b2fb39a7391263a05e85a
Sunil Kumnar <sk@lnk7.com>
2019-09-23 17:40:28 -0500

    Improved Ajax handling. Added Request And Response Classes.

---
+++ src/Library/Request.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Lnk7\Genie\Library;
+
+use Lnk7\Genie\Tools;
+
+class Request {
+
+    /**
+     * Input variables
+     *
+     * @var null
+     */
+    private static $data = null;
+
+
+
+    /**
+     * Get a value from the input
+     *
+     * @param $var
+     *
+     * @return bool|mixed
+     */
+    public static function get( $var ) {
+        static::maybeParseInput();
+        if ( isset( static::$data[ $var ] ) ) {
+            return static::$data[ $var ];
+        }
+
+        return false;
+
+    }
+
+
+
+    /**
+     * Collect Data from various input mechanisms
+     */
+    private static function maybeParseInput() {
+
+        if ( ! is_null( static::$data ) ) {
+            return;
+        }
+
+        static::$data = [];
+
+        $body = file_get_contents( 'php://input' );
+
+        if ( $body ) {
+            static::$data = array_merge( static::$data, json_decode( $body, true ) );
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                Response::Failure('Invalid json');
+            }
+        }
+
+        if ( ! empty( $_GET ) ) {
+            static::$data = array_merge( static::$data, Tools::stripSlashesArray( $_GET ) );
+        }
+
+        if ( ! empty( $_POST ) ) {
+            static::$data = array_merge( static::$data, Tools::stripSlashesArray( $_POST ) );
+        }
+
+        return;
+    }
+
+}
--- src/Tools.php
+++ src/Tools.php
@@ -2,181 +2,148 @@
 
 namespace Lnk7\Genie;
 
-use Lnk7\Genie\WordPressObjects\Page;
-
 /**
  * Class Tools
  * @package Lnk7\Genie
  */
 class Tools {
 
-	/**
-	 * Add slashes to a string
-	 *
-	 * @param        $string
-	 * @param string $chars
-	 *
-	 * @return string
-	 */
-	public static function addSlashes( $string, $chars = '"' ) {
+    /**
+     * Add slashes to a string
+     *
+     * @param        $string
+     * @param string $chars
+     *
+     * @return string
+     */
+    public static function addSlashes( $string, $chars = '"' ) {
 
-		return addcslashes( $string, $chars );
+        return addcslashes( $string, $chars );
 
-	}
+    }
 
+    public static function stripSlashesArray( $value ) {
+        $value = is_array( $value ) ?
+            array_map( 'stripslashes_deep', $value ) :
+            stripslashes( $value );
 
-
-	/**
-	 * Dump a variable to the console
-	 *
-	 * @param $var
-	 */
-	public static function console( $var ) {
-
-		if ( is_array( $var ) or is_object( $var ) ) {
-			$var = print_r( $var, true );
-		}
-		print "<script>console.log(" . json_encode( $var ) . ")</script>";
-	}
+        return $value;
+    }
 
 
+    /**
+     * Dump a variable to the console
+     *
+     * @param $var
+     */
+    public static function console( $var ) {
 
-	/**
-	 * Dump a variable
-	 *
-	 * @param $var
-	 */
-	public static function d( $var ) {
-
-		if ( is_array( $var ) or is_object( $var ) ) {
-			$var = print_r( $var, true );
-		}
-		print "<pre>$var</pre>";
-	}
+        if ( is_array( $var ) or is_object( $var ) ) {
+            $var = print_r( $var, true );
+        }
+        print "<script>console.log(" . json_encode( $var ) . ")</script>";
+    }
 
 
 
-	/**
-	 * Dump and die
-	 *
-	 * @param $var
-	 */
-	public static function dd( $var ) {
+    /**
+     * Dump and die
+     *
+     * @param $var
+     */
+    public static function dd( $var ) {
 
-		self::d( $var );
-		exit;
-	}
-
-
-
-	public static function formatBytes( $size, $precision = 0 ) {
-
-		$base     = log( $size, 1024 );
-		$suffixes = [ '', 'K', 'M', 'G', 'T' ];
-
-		return round( pow( 1024, $base - floor( $base ) ), $precision ) . $suffixes[ floor( $base ) ];
-	}
+        self::d( $var );
+        exit;
+    }
 
 
 
-	/**
-	 * Extract the domain name from a url
-	 *
-	 * @param $url
-	 *
-	 * @return bool
-	 */
-	public static function getDomainName( $url ) {
+    /**
+     * Dump a variable
+     *
+     * @param $var
+     */
+    public static function d( $var ) {
 
-		$pieces = parse_url( $url );
-		$domain = isset( $pieces['host'] ) ? $pieces['host'] : $pieces['path'];
-		if ( preg_match( '/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs ) ) {
-			return $regs['domain'];
-		}
-
-		return false;
-	}
+        if ( is_array( $var ) or is_object( $var ) ) {
+            $var = print_r( $var, true );
+        }
+        print "<pre>$var</pre>";
+    }
 
 
 
-	/**
-	 * Get an IP Address
-	 * @return mixed
-	 */
-	public static function getIpAddress() {
+    /**
+     * Extract the domain name from a url
+     *
+     * @param $url
+     *
+     * @return bool
+     */
+    public static function getDomainName( $url ) {
 
-		return $_SERVER['REMOTE_ADDR'];
-	}
+        $pieces = parse_url( $url );
+        $domain = isset( $pieces['host'] ) ? $pieces['host'] : $pieces['path'];
+        if ( preg_match( '/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs ) ) {
+            return $regs['domain'];
+        }
 
-
-
-
-
-	/**
-	 * Pick up headers
-	 */
-	public static function getRequestHeaders() {
-
-		$headers = [];
-		foreach ( $_SERVER as $key => $value ) {
-			if ( substr( $key, 0, 5 ) <> 'HTTP_' ) {
-				continue;
-			}
-			$header             = str_replace( ' ', '-', ucwords( str_replace( '_', ' ', strtolower( substr( $key, 5 ) ) ) ) );
-			$headers[ $header ] = $value;
-		}
-
-		return $headers;
-	}
+        return false;
+    }
 
 
 
-	/**
-	 * Check if the current user is a Site administrator
-	 *
-	 * @return boolean
-	 */
-	public static function isSiteAdmin() {
+    /**
+     * Get an IP Address
+     * @return mixed
+     */
+    public static function getIpAddress() {
 
-		if ( ! is_user_logged_in() ) {
-			return false;
-		}
-
-		return in_array( 'administrator', wp_get_current_user()->roles );
-	}
+        return $_SERVER['REMOTE_ADDR'];
+    }
 
 
 
-	public static function jsonSafe( $data ) {
+    /**
+     * Pick up headers
+     */
+    public static function getRequestHeaders() {
 
-		return str_replace( "'", '&apos;', json_encode( $data ) );
+        $headers = [];
+        foreach ( $_SERVER as $key => $value ) {
+            if ( substr( $key, 0, 5 ) <> 'HTTP_' ) {
+                continue;
+            }
+            $header             = str_replace( ' ', '-', ucwords( str_replace( '_', ' ', strtolower( substr( $key, 5 ) ) ) ) );
+            $headers[ $header ] = $value;
+        }
 
-	}
+        return $headers;
+    }
 
 
 
-	/**
-	 * Check is url exists
-	 *
-	 * @param $url
-	 *
-	 * @return bool
-	 */
-	function urlExists( $url ) {
+    /**
+     * Check if the current user is a Site administrator
+     *
+     * @return boolean
+     */
+    public static function isSiteAdmin() {
 
-		$transient = 'genie_urlExists_' . md5( $url );
+        if ( ! is_user_logged_in() ) {
+            return false;
+        }
 
-		if ( false === ( $exists = get_transient( $transient ) ) ) {
-			$headers = @get_headers( $url );
-			if ( ! $headers || strpos( $headers[0], '404' ) ) {
-				$exists = 'no';
-			} else {
-				$exists = 'yes';
-			}
-			set_transient( $transient, $exists, 12 * HOUR_IN_SECONDS );
-		}
+        return in_array( 'administrator', wp_get_current_user()->roles );
+    }
 
-		return $exists == 'yes';
-	}
+
+
+    public static function jsonSafe( $data ) {
+
+        return str_replace( "'", '&apos;', json_encode( $data ) );
+
+    }
 
 }
