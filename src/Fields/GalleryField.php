958954c35cc8decd02cd49c6d3fa2e306c30a565
Sunil Jaiswal <sunil@lnk7.com>
2020-09-04 10:45:47 +0100

    Added Gallery Field

---
+++ src/Fields/GalleryField.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Lnk7\Genie\Fields;
+
+class GalleryField extends ImageField {
+
+    protected $type = 'gallery';
+
+
+
+    /**
+     * Specify the maximum posts allowed to be selected. Defaults to 0
+     *
+     * @param int $number
+     *
+     * @return $this
+     */
+    public function max( int $number ) {
+
+        return $this->set( 'max', $number );
+
+    }
+
+
+
+    /**
+     * Specify the minimum posts required to be selected. Defaults to 0
+     *
+     * @param int $number
+     *
+     * @return $this
+     */
+    public function min( int $number ) {
+
+        return $this->set( 'min', $number );
+
+    }
+
+
+
+    /**
+     * Set defaults for this field
+     */
+    protected function setDefaults() {
+        parent::setDefaults();
+        $this->insert( 'append' );
+    }
+
+
+
+    /**
+     * Where to insert the image
+     *
+     * @param string $insert append|prepend
+     *
+     * @return $this
+     */
+    public function insert( string $insert ) {
+        return $this->set( 'insert', $insert );
+
+    }
+
+}
