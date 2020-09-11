95b52bf2bb0499976a8ca432574046d795a6f460
Sunil Jaiswal <sunil@lnk7.com>
2020-09-11 19:10:04 +0100

    Added ApiHandler

---
+++ src/ApiHandler.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Lnk7\Genie;
+
+use Lnk7\Genie\Interfaces\GenieComponent;
+use Lnk7\Genie\Utilities\HookInto;
+use Throwable;
+
+/**
+ * Class ApiHandler
+ *
+ * @package Lnk7\Genie
+ */
+class ApiHandler implements GenieComponent
+{
+
+
+    /**
+     * An array of paths to use for ajax calls
+     *
+     * @var array
+     */
+    protected static $routes = [];
+
+
+    /**
+     * Setup Actions, Filters and Shortcodes
+     */
+    public static function setup()
+    {
+
+        /**
+         * Handle the ajax call.
+         */
+        HookInto::action('init')
+            ->run(function () {
+                $path = apply_filters('genie_api_path', 'api');
+                $action = apply_filters('genie_api_action', 'genie_api');
+                add_rewrite_rule($path . '/(.*)$', 'wp-admin/admin-ajax.php?action=' . $action . '&route=$1', 'top');
+
+                HookInto::action('wp_ajax_' . $action)
+                    ->orAction('wp_ajax_nopriv_' . $action)
+                    ->run(function () {
+
+                        Request::maybeParseInput();
+
+                        $route = Request::get('route');
+
+                        if (!$route) {
+                            Response::error([
+                                'message' => "No request specified",
+                            ]);
+                        }
+
+                        if (!static::canHandle($route)) {
+                            Response::notFound([
+                                'message' => "Request: {$route} not found",
+                            ]);
+                        }
+
+                        // The Callback exists
+                        $callback = static::$routes[$route]->callback;
+                        $method = static::$routes[$route]->method;
+
+                        if ($method !== Request::method()) {
+                            Response::error([
+                                'message' => "This route does not support ".Request::method()." requests",
+                            ]);
+                        }
+
+                        $params = Tools::getCallableParameters($callback);
+
+                        if (is_wp_error($params)) {
+                            Response::error([
+                                'message' => $params->get_error_message(),
+                            ]);
+                        }
+
+                        if (Request::wasJsonReceived() && Request::wasJsonReceivedInvalid()) {
+                            Response::error([
+                                'message' => "Invalid json received",
+                            ]);
+                        }
+
+                        try {
+
+                            $callbackParams = [];
+
+                            foreach ($params as $param) {
+                                $name = $param->getName();
+                                $value = Request::get($name);
+                                if (!$param->isOptional() and !isset($value)) {
+                                    Response::failure(['message' => "required parameter {$name} is missing"]);
+                                }
+                                $callbackParams[$name] = $value;
+                            }
+                            Response::success(call_user_func_array($callback, $callbackParams));
+
+                        } catch (Throwable $e) {
+
+                            Response::failure([
+                                'message' => $e->getMessage(),
+                            ]);
+                        }
+                    });
+            });
+
+    }
+
+
+    /**
+     * Check that a path is registered
+     *
+     * @param $route
+     *
+     * @return bool
+     */
+    public static function canHandle($route)
+    {
+        return array_key_exists($route, static::$routes);
+    }
+
+
+    /**
+     * Register an ajax callback function
+     *
+     * @param string $path
+     * @param string $method
+     * @param callable $callback
+     */
+    public static function register(string $path, string $method, callable $callback)
+    {
+        static::$routes[$path] = (object)[
+            'method'   => strtoupper(trim($method)),
+            'callback' => $callback,
+        ];
+    }
+
+}
