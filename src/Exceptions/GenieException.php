77c79d0bac16a73eb68f28a5a01a90cfc0b0eb3d
Sunil Jaiswal <sunil@lnk7.com>
2020-09-17 20:34:01 +0100

    Added data to GenieException.php

--- src/Exceptions/GenieException.php
+++ src/Exceptions/GenieException.php
@@ -9,10 +9,79 @@ class GenieException extends Exception
 {
 
 
+    protected $data = null;
+
+
     function __construct($message = "", $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
         do_action('genie_exception', $this);
+    }
+
+
+    /**
+     * Static constructor
+     *
+     * @param $message
+     * @param null $data
+     *
+     * @return static
+     */
+    public static function withMessage($message, $data = null)
+    {
+        $error = new static($message);
+        $error->data = $data;
+        return $error;
+
+    }
+
+
+    /**
+     * throw this error
+     *
+     * @throws GenieException
+     */
+    public function throw()
+    {
+        throw $this;
+
+    }
+
+
+    /**
+     * does this error message have data?
+     *
+     * @return bool
+     */
+    public function hasData()
+    {
+        return !is_null($this->data);
+    }
+
+
+    /**
+     * Add data to this exception
+     *
+     * @param $data
+     *
+     * @return $this
+     */
+    public function withData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+
+    /**
+     * get the data for this exception
+     *
+     * @return mixed
+     */
+    function getData()
+    {
+        return $this->data;
     }
 
 }
